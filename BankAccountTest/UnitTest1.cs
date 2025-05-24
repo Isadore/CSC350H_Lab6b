e03ee3822c1b8695673c9836646abdd2d77f8eed
Isadore <remyrascal@live.com>
2025-05-24 19:12:23 -0400

    finished test init and and test deposit

--- BankAccountTest/UnitTest1.cs
+++ BankAccountTest/UnitTest1.cs
@@ -6,16 +6,20 @@ public class UnitTest1
     [TestMethod]
     public void TestInitialization()
     {
-        var account = new BankAccount("Test", 3, 0);
+        BankAccount account = new BankAccount("Test", 3, 0);
         Assert.AreEqual(0, account.Balance);
         Assert.AreEqual(0, account.TransactionCount);
         Assert.AreEqual("Test", account.CustomerName);
         Assert.AreEqual(3, account.Id);
+        var account2 = new BankAccount("Test1", 4, 123.40);
+        Assert.AreEqual(123.40, account2.Balance);
+        Assert.AreEqual(4, account2.Id);
     }
 
     [TestMethod]
-    public void TestWithdrawal() {
-        var account = new BankAccount("Test", 3, 50);
+    public void TestWithdrawal()
+    {
+        BankAccount account = new BankAccount("Test", 3, 50);
         Assert.AreEqual(50, account.Balance);
         account.withdraw(23.50);
         account.withdraw(.50);
@@ -23,5 +27,14 @@ public class UnitTest1
         Assert.ThrowsException<Exception>(() => account.withdraw(27));
     }
 
+    [TestMethod]
+    public void TestDeposit()
+    {
+        BankAccount account = new BankAccount("Test", 5, 75);
+        Assert.AreEqual(75, account.Balance);
+        account.withdraw(23.50);
+        Assert.AreEqual(51.50, account.Balance);
+        Assert.ThrowsException<Exception>(() => account.withdraw(65));
+    }
 
 }
